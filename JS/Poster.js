e29dd48d67852b785bf618a6122066b4317fe16e
Vladik <Kostyuchenko_V@it.belstu.by>
2023-05-03 03:29:33 +0300

    new file:   JS/MainPage.js 	new file:   JS/Poster.js 	deleted:    JS/main.js 	modified:   SCSS/main.css 	modified:   SCSS/main.css.map 	modified:   SCSS/main.scss 	new file:   SVG/cross.svg 	new file:   films/1+1.webp 	new file:   films/aferisty.jpg 	new file:   films/bestoloch.jpg 	new file:   films/bez-otveta.jpg 	new file:   films/bezos.jpg 	new file:   films/boiicsovskiy_club.webp 	new file:   films/doctor_lisa.webp 	new file:   films/dysha.webp 	new file:   films/dzhon-uik.jpg 	deleted:    films/film_beschenstvo.png 	deleted:    films/film_marlou.png 	deleted:    films/film_rycsarteney.png 	deleted:    films/film_syn.png 	new file:   films/forest_gamp.webp 	new file:   films/interstellar.webp 	new file:   films/korol_lev.webp 	new file:   films/korporativ.jpg 	new file:   films/mizantrop.jpg 	new file:   films/nazad_v_buduschie.webp 	new file:   films/pobeg_is_shoushenka.webp 	new file:   films/renfild.jpg 	new file:   films/spisok_shindlera.webp 	new file:   films/tayna_koko.webp 	new file:   films/tyomnyi_rycar.webp 	new file:   films/udacha.jpg 	new file:   films/vyzov.jpg 	new file:   films/yaga.jpg 	new file:   films/zelonaya_kniga.webp 	renamed:    films/300x450.webp -> films/zelonaya_mila.webp 	new file:   films/zveropoy2.webp 	deleted:    image/film_beschenstvo.png 	deleted:    image/film_marlou.png 	deleted:    image/film_rycsarteney.png 	deleted:    image/film_syn.png 	deleted:    mainxml.xml 	new file:   parsexml1.xml 	new file:   parsexml2.xml 	new file:   parsexml3.xml 	modified:   "\320\220\321\204\320\270\321\210\320\260.html" 	modified:   "\320\223\320\273\320\260\320\262\320\275\320\260\321\217.html"

---
+++ JS/Poster.js
@@ -0,0 +1,273 @@
+//slider
+const slides = document.querySelector('.slides');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let screen = document.documentElement.clientWidth;
+let slideWidth;
+
+if(screen >= 1320)
+{
+  slideWidth = slides.clientWidth / 3.1;
+  let currentPosition = 0;
+
+prevButton.addEventListener('click', () => {
+  currentPosition += slideWidth;
+  if (currentPosition > 0) {
+    currentPosition = -(slideWidth * 7);
+  }
+  slides.style.transform = `translateX(${currentPosition}px)`;
+});
+
+nextButton.addEventListener('click', () => {
+  currentPosition -= slideWidth;
+  if (currentPosition < -(slideWidth * 7)) {
+    currentPosition = 0;
+  }
+  slides.style.transform = `translateX(${currentPosition}px)`;
+});
+
+}
+else if(screen >= 800 && screen < 1320)
+{
+  slideWidth = slides.clientWidth / 2;
+  let currentPosition = 0;
+
+prevButton.addEventListener('click', () => {
+  currentPosition += slideWidth;
+  if (currentPosition > 0) {
+    currentPosition = -(slideWidth * 8);
+  }
+  slides.style.transform = `translateX(${currentPosition}px)`;
+});
+
+nextButton.addEventListener('click', () => {
+  currentPosition -= slideWidth;
+  if (currentPosition < -(slideWidth * 8)) {
+    currentPosition = 0;
+  }
+  slides.style.transform = `translateX(${currentPosition}px)`;
+});
+
+}
+else
+{
+  slideWidth = slides.clientWidth ;
+  let currentPosition = 0;
+
+prevButton.addEventListener('click', () => {
+  currentPosition += slideWidth;
+  if (currentPosition > 0) {
+    currentPosition = -(slideWidth * 9);
+  }
+  slides.style.transform = `translateX(${currentPosition}px)`;
+});
+
+nextButton.addEventListener('click', () => {
+  currentPosition -= slideWidth;
+  if (currentPosition < -(slideWidth * 9)) {
+    currentPosition = 0;
+  }
+  slides.style.transform = `translateX(${currentPosition}px)`;
+});
+
+}
+
+
+
+
+
+
+
+
+
+let table2 = (arr_tag, arr) => {
+  let i = 0;
+  for (const elem_arr of arr_tag) {
+    arr[i++] = elem_arr.textContent;
+  }
+  return arr;
+}
+
+let parse2 = (xmlString => {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+  const names = xmlDoc.getElementsByTagName('name');
+  const images = xmlDoc.getElementsByTagName('image');
+
+  let names_arr = [], images_arr = [];
+  names_arr = table2(names, names_arr);
+  images_arr = table2(images, images_arr);
+
+
+  for (let i = 0; i < names_arr.length; i++) {
+
+    document.querySelector('.slides').insertAdjacentHTML("beforeend",
+
+      `<div class="imgout">
+      <p>${names_arr[i]}</p>
+      <img src="${images_arr[i]}"/>
+        </div>`
+    );
+
+  }
+    
+}
+
+)
+
+async function parseXML2() {//async ключевое слово которое обозначает что наша функция становиться асинхронной
+  let a = await fetch('../parsexml3.xml') // await ключевое слово которое грубо говорят останавливает наш весь следующий код, пока запрос не выполнится
+  let xmlString = await a.text();//та же песня как только мы приобразуем наши данные в текст мы можем их использовать
+  parse2(xmlString);//вызываем функции которая все парсит, ее реализация сверху
+}
+
+parseXML2();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function byfilmas(){
+  const showFormButtons = document.querySelectorAll('.buyticket');
+const ticketForm = document.getElementById('ticket-form');
+const closeButton = document.querySelector('.close-button');
+const overlay = document.getElementById('overlay');
+
+showFormButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    ticketForm.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+const buyButton = document.getElementById('buy');
+
+buyButton.addEventListener('click', () => {
+  const name = document.getElementById('name').value;
+  const cinema = document.getElementById('cinema').value;
+  const date = document.getElementById('date').value;
+  const time = document.getElementById('time').value;
+  const row = document.getElementById('row').value;
+  const seat = document.getElementById('seat').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+  const payment = document.querySelector('input[name="payment"]:checked').value;
+  
+  document.getElementById('name').value = '';
+  document.getElementById('cinema').selectedIndex = 0;
+  document.getElementById('date').value = '';
+  document.getElementById('time').selectedIndex = 0;
+  document.getElementById('row').selectedIndex = 0;
+  document.getElementById('seat').selectedIndex = 0;
+  document.getElementById('phone').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('card').checked = false;
+  document.getElementById('cash').checked = false;
+  ticketForm.classList.add('hidden'); // скрываем окно с формой
+  overlay.classList.add('hidden');
+  // здесь можно добавить логику для обработки покупки билета
+});
+
+// добавляем обработчик события для кнопки закрытия
+closeButton.addEventListener('click', () => {
+  ticketForm.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+overlay.addEventListener('click', () => {
+  ticketForm.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+// добавляем обработчик события для скрытия формы при клике за ее пределами
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('#ticket-form') && !event.target.classList.contains('buyticket')) {
+    ticketForm.classList.add('hidden');
+  }
+});
+}
+
+//XML
+
+let table = (arr_tag, arr) => {
+  let i = 0;
+  for (const elem_arr of arr_tag) {
+    arr[i++] = elem_arr.textContent;
+  }
+  return arr;
+}
+
+let parse = (xmlString => {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+  const names = xmlDoc.getElementsByTagName('name');
+  const images = xmlDoc.getElementsByTagName('image');
+  const types = xmlDoc.getElementsByTagName('type');
+  const times = xmlDoc.getElementsByTagName('time');
+  const countrys_ages = xmlDoc.getElementsByTagName('country_age');
+  const genres = xmlDoc.getElementsByTagName('genre');
+  const dates = xmlDoc.getElementsByTagName('date');
+
+  let names_arr = [], images_arr = [], types_arr = [],
+    times_arr = [], countrys_ages_arr = [], genres_arr = [], dates_arr = [];
+  names_arr = table(names, names_arr);
+  images_arr = table(images, images_arr);
+  types_arr = table(types, types_arr);
+  times_arr = table(times, times_arr);
+  countrys_ages_arr = table(countrys_ages, countrys_ages_arr);
+  genres_arr = table(genres, genres_arr);
+  dates_arr = table(dates, dates_arr);
+
+  for (let i = 0; i < names_arr.length; i++) {
+
+    document.querySelector('.films').insertAdjacentHTML("beforeend",
+
+      `<div class="film">
+      <p>${names_arr[i]}</p>
+      <div class="img_film">
+          <div class="on_img">
+              <p class="type">${types_arr[i]}</p>
+              <p class="time">${times_arr[i]}</p>
+              <p class="country_age">${countrys_ages_arr[i]}</p>
+              <p class="genre">${genres_arr[i]}</p>
+              <p class="date">${dates_arr[i]}</p>   
+              <button class="buyticket">Купить билет</button>
+          </div> 
+      </div>
+        </div>`
+
+    );
+
+    document.querySelectorAll('.img_film')[i].style.background = `url(${images_arr[i]})`;
+  }
+  byfilmas();
+}
+
+)
+
+async function parseXML() {//async ключевое слово которое обозначает что наша функция становиться асинхронной
+  let a = await fetch('../parsexml2.xml') // await ключевое слово которое грубо говорят останавливает наш весь следующий код, пока запрос не выполнится
+  let xmlString = await a.text();//та же песня как только мы приобразуем наши данные в текст мы можем их использовать
+  parse(xmlString);//вызываем функции которая все парсит, ее реализация сверху
+}
+
+parseXML();
+
+
+
+
+
+
+
